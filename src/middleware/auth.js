06f2bfc2b61237f5a7df22e68d0aa77e1bf9c7bc
waffqh <faiqawafafiqh@gmail>
2026-06-03 15:41:55 +0700

    update env

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,30 +1,30 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY;
   if (!authHeader) {
     return res.status(401).json({
-      status: 'failed',
-      message: 'Unauthorized',
+      status: "failed",
+      message: "Unauthorized",
     });
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(
-      token,
-      'accesssecret'
-    );
+    const decoded = jwt.verify(token, ACCESS_TOKEN_KEY);
 
     req.user = decoded;
 
     next();
   } catch (error) {
     return res.status(401).json({
-      status: 'failed',
-      message: 'Invalid token',
+      status: "failed",
+      message: "Invalid token",
     });
   }
 };
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,45 +1,37 @@
-import pool from '../config/database.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import pool from "../config/database.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const login = async ({ email, password }) => {
+  const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY;
+  const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN_KEY;
   const query = {
-    text: 'SELECT * FROM users WHERE email = $1',
+    text: "SELECT * FROM users WHERE email = $1",
     values: [email],
   };
 
   const result = await pool.query(query);
 
   if (!result.rows.length) {
-    throw new Error('User tidak ditemukan');
+    throw new Error("User tidak ditemukan");
   }
 
   const user = result.rows[0];
 
-  const match = await bcrypt.compare(
-    password,
-    user.password
-  );
+  const match = await bcrypt.compare(password, user.password);
 
   if (!match) {
-    throw new Error('Password salah');
+    throw new Error("Password salah");
   }
 
-  const accessToken = jwt.sign(
-    { id: user.id },
-    'accesssecret',
-    {
-      expiresIn: '3h',
-    }
-  );
+  const accessToken = jwt.sign({ id: user.id }, ACCESS_TOKEN_KEY, {
+    expiresIn: "3h",
+  });
 
-  const refreshToken = jwt.sign(
-    { id: user.id },
-    'refreshsecret'
-  );
+  const refreshToken = jwt.sign({ id: user.id }, REFRESH_TOKEN_KEY);
 
   await pool.query({
     text: `
